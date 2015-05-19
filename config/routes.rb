a7c10c8eebe8f5548f05ee25b52d6bfe7364061b
Adam Pash <adam.pash@gmail.com>
2015-05-19 10:49:02 -0400

    Added pages controller for various one-offs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
   # devise_for :users
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
