c4e2c33ff6df707a65500a394434afb5b7174e40
Adam Pash <adam.pash@gmail.com>
2015-05-21 15:51:21 -0400

    some basic structure + login/out

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   # devise_for :users
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do
+    get "/logout" => "devise/sessions#destroy"
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
