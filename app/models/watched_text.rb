30c5aa7e4acd3bea55ee952ab1935e6d523a2b12
Adam Pash <adam.pash@gmail.com>
2015-10-28 16:23:05 -0400

    setting to notify user if user has slack username set

--- app/models/watched_text.rb
+++ app/models/watched_text.rb
@@ -1,9 +1,16 @@
 class WatchedText < ActiveRecord::Base
   belongs_to :user
   belongs_to :text
+  before_save :set_notification
   validate :unique_pair
 
   protected
+  def set_notification
+    if user.slack_username
+      self.notify = true
+    end
+  end
+
   def unique_pair
     !self.class.exists?(
         :user_id => user_id,
