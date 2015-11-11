f661e1813026ccf179f92a1d829b40c7f6c220b6
Adam Pash <adam.pash@gmail.com>
2015-11-11 12:42:26 -0500

    added proper url

--- lib/notifier.rb
+++ lib/notifier.rb
@@ -4,10 +4,11 @@ module Notifier
       user = watched_text.user
       if should_send?(watched_text) and has_slack?(user)
         puts "Should notify #{user.slack_username} for #{text.url}"
+        url = Rails.application.routes.url_helpers(text, host: 'todiffer.herokuapp.com')
         notifier = Slack::Notifier.new ENV["SLACK_WEBHOOK"],
           channel: "@#{user.slack_username}",
           username: "ToDifferBot"
-        notifier.ping "New update to #{text.url}"
+        notifier.ping "New update to #{url}"
       end
     end
   end
